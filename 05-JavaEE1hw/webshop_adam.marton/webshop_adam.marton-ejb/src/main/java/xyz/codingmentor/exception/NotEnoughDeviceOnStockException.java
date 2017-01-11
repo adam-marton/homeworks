09e793070a3f8922602a82a1a9fb23c4742fd733
Marton Adam <martonadam66@gmail.com>
2017-01-11 16:53:27 +0100

    Modify ShoppingCart class, add Exception

---
+++ 05-JavaEE1hw/webshop_adam.marton/webshop_adam.marton-ejb/src/main/java/xyz/codingmentor/exception/NotEnoughDeviceOnStockException.java
@@ -0,0 +1,16 @@
+package xyz.codingmentor.exception;
+
+/**
+ *
+ * @author Ádám
+ */
+public class NotEnoughDeviceOnStockException extends RuntimeException {
+
+    public NotEnoughDeviceOnStockException() {
+        //default constructor
+    }
+
+    public NotEnoughDeviceOnStockException(String message) {
+        super(message);
+    }
+}
--- 05-JavaEE1hw/webshop_adam.marton/webshop_adam.marton-ejb/src/main/java/xyz/codingmentor/shoppingcart/ShoppingCart.java
+++ 05-JavaEE1hw/webshop_adam.marton/webshop_adam.marton-ejb/src/main/java/xyz/codingmentor/shoppingcart/ShoppingCart.java
@@ -6,6 +6,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import xyz.codingmentor.database.DeviceDB;
 import xyz.codingmentor.entity.DeviceEntity;
+import xyz.codingmentor.exception.NotEnoughDeviceOnStockException;
 
 /**
  *
@@ -17,27 +18,36 @@ public class ShoppingCart {
     private Integer value;
     
     public void addDeviceToCart(String id, Integer quantity) {
-        devicesInCart.put(DeviceDB.getInstance().getDevice(id), quantity);
-        DeviceDB.getInstance()
-                .getDevice(id)
-                .setCount(DeviceDB.getInstance().getDevice(id).getCount() - quantity);
-        value += DeviceDB.getInstance().getDevice(id).getPrice();
+        DeviceEntity device = DeviceDB.getInstance().getDevice(id);
+        if(quantity <= device.getCount()) {
+            devicesInCart.put(device, quantity);
+            device.setCount(device.getCount() - quantity);
+            DeviceDB.getInstance().editDevice(device);
+            value += quantity * device.getPrice();
+        }
+        throw new NotEnoughDeviceOnStockException("Not enough " 
+                + device.getManufacturer() + ", " + device.getType() + " on stock.");
     }
     
     public void deleteDeviceFromCart(String id, Integer quantity) {
-        if(quantity < devicesInCart.get(DeviceDB.getInstance().getDevice(id))) {
-            devicesInCart.replace(DeviceDB.getInstance().getDevice(id), 
-                    devicesInCart.get(DeviceDB.getInstance().getDevice(id)) - quantity);
+        DeviceEntity device = DeviceDB.getInstance().getDevice(id);
+        if(quantity < devicesInCart.get(device)) {
+            devicesInCart.replace(device, devicesInCart.get(device) - quantity);
+            value -= quantity * device.getPrice();
+            device.setCount(device.getCount() + quantity);
+            DeviceDB.getInstance().editDevice(device);
         }
-        devicesInCart.remove(DeviceDB.getInstance().getDevice(id));
-        value -= DeviceDB.getInstance().getDevice(id).getPrice();
+        value -= devicesInCart.get(device) * device.getPrice();
+        device.setCount(device.getCount() + quantity);
+        DeviceDB.getInstance().editDevice(device);
+        devicesInCart.remove(device);
     }
     
     public void deleteAllDevicesFromCart() {
-        for(Map.Entry<DeviceEntity, Integer> device : devicesInCart.entrySet()) {
-            DeviceDB.getInstance()
-                    .getDevice(device.getKey().getId())
-                    .setCount(DeviceDB.getInstance().getDevice(device.getKey().getId()).getCount() + device.getValue());
+        for(Map.Entry<DeviceEntity, Integer> d : devicesInCart.entrySet()) {
+            DeviceEntity device = DeviceDB.getInstance().getDevice(d.getKey().getId());
+            device.setCount(device.getCount() + d.getValue());
+            DeviceDB.getInstance().editDevice(device);
         }
         devicesInCart.clear();
         value = 0;
@@ -49,10 +59,10 @@ public class ShoppingCart {
     
     public void buyDevicesInCart() {
         StringBuilder stringBuilder = new StringBuilder();
-        for(Map.Entry<DeviceEntity, Integer> device : devicesInCart.entrySet()) {
-            stringBuilder.append(device.getKey().toString())
-                    .append("Quantity: ")
-                    .append(device.getValue());
+        for(Map.Entry<DeviceEntity, Integer> d : devicesInCart.entrySet()) {
+            stringBuilder.append(d.getKey().toString())
+                    .append("\nQuantity: ")
+                    .append(d.getValue());
         }
         LOGGER.log(Level.INFO, stringBuilder.toString());
         LOGGER.log(Level.INFO, "Cart value: {0}", getCartValue().toString());
